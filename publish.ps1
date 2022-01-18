500f6fbcdf47f28639e087313986b817dc679c22
Johannes Egger <j.egger@posteo.at>
2022-01-18 22:26:31 +0100

    Simplify publish script

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,7 @@
-$outputDir = "$PSScriptRoot\deploy"
-dotnet publish .\src\server -c Release -o $outputDir
+Push-Location $PSScriptRoot
+
+dotnet publish .\src\server -c Release -o .\deploy
 yarn --cwd .\src\client install --frozen-lockfile
-yarn --cwd .\src\client webpack --output-path "$outputDir\wwwroot"
+yarn --cwd .\src\client webpack --output-path "$pwd\deploy\wwwroot"
+
+Pop-Location
